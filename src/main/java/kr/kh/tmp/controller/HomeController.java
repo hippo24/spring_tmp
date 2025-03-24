ac37b52e5ec5d9904712664ce481789cc7c476b9
hippo24 <lodenx24@gmail.com>
2025-03-24 11:01:55 +0900

    DB 연동 완료

--- src/main/java/kr/kh/tmp/controller/HomeController.java
+++ src/main/java/kr/kh/tmp/controller/HomeController.java
@@ -1,11 +1,15 @@
 package kr.kh.tmp.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
 public class HomeController {
+	
+	@Autowired
+	private MemberSerivce memberService;
 	
 	@RequestMapping(value = "/", method = RequestMethod.GET)
 	public String home() {
